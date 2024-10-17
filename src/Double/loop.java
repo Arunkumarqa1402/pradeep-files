7bab7c68368175939c14e7dfb5760807c413da4c
syss <syss@sys>
2024-10-17 14:08:52 +0530

    hi pradeep

---
+++ src/Double/loop.java
@@ -0,0 +1,33 @@
+package Double;
+
+public class loop {
+	
+	
+	void loops ()
+	{
+	for(int a=0;a<5;a++)
+
+	{
+
+	for(int b=0;b<5;b++)
+	{
+		
+
+	System.out.println(" i am single for"+a );
+		
+	System.out.println(" i am double for"+b);
+		
+	}
+	}
+	
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+loop obj=new loop();
+obj.loops();
+		
+	}
+
+}
---
+++ src/loopings/number.java
@@ -0,0 +1,26 @@
+package loopings;
+
+public class number {
+	
+	
+	void loops()
+	{
+		
+		for(int a=5;a<=100;a++)
+		{
+			
+			System.out.println(a);
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		number obj=new number();
+		
+		obj.loops();
+	}
+
+}
